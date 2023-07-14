b5dc0b0081a5b0ed96c4b26a71f37a1d7eefb78a
MartinSembrat <MartinSembrat@gmail.com>
2023-07-14 12:36:42 +0200

    start new repo

---
+++ src/main/java/MassBalanceCalculator/MassBalanceCalculatorApplication.java
@@ -0,0 +1,13 @@
+package MassBalanceCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassBalanceCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassBalanceCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MassBalanceCalculator/MassBalanceCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package MassBalanceCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassBalanceCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
